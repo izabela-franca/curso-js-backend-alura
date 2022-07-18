a89abc393bc9f35bfbee11c19fb330d0336fff3a
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2022-07-18 20:25:15 -0300

    Add files via upload

---
+++ Curso3_JavaScript_Objetos/manipulandoChavesObjetos.js
@@ -0,0 +1,17 @@
+const cliente = {
+    nome: "Izabela",
+    idade: 31,
+    cpf: "12345678901",
+    email: "izabela@email.com"
+}
+
+//Adicionando novas chaves a um objeto
+cliente.telefone = "947856312"
+
+
+//Alterando uma chave
+cliente.telefone = "987654321";
+
+
+//Removendo uma chave
+delete cliente.telefone;
