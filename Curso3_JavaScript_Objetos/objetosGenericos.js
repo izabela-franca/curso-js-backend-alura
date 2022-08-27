b50d965bf766dd41cf1390c7b73ad07047bf4ab9
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2022-08-27 08:43:08 -0300

    Add files via upload

---
+++ Curso3_JavaScript_Objetos/objetosGenericos.js
@@ -0,0 +1,16 @@
+//Declarando uma função construtora
+function Cliente (nome, cpf, email, saldo) {
+    this.nome = nome,
+    this.cpf = cpf,
+    this.email = email,
+    this.saldo = saldo
+    this.depositar = function (valor){      //método depositar
+        this.salado += valor
+    }
+}
+
+
+//Criando um novo objeto
+const izabela = new Cliente ("Izabela França", "41060428126", "izabela@email.com", 250)
+
+console.log (izabela)
