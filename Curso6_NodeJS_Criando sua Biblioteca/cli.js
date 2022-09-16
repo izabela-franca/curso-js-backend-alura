6f67e10f2a7dab2d92f5357f326dfce8f3214f6a
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2022-09-16 19:12:50 -0300

    Update cli.js

--- Curso6_NodeJS_Criando sua Biblioteca/cli.js
+++ Curso6_NodeJS_Criando sua Biblioteca/cli.js
@@ -5,4 +5,10 @@ import { acessarArquivo } from "./index";
 
 const caminho = process.argv;
 
+function processarTexto(caminhoArquivo) {
+    const resultado = acessarArquivo(caminhoArquivo[2]);
+    console.log(("lista de links"), resultado);
+}
+
+processarTexto (caminho);
 console.log(acessarArquivo(caminho[2]));
