f9fe95b8762f3431ad2fd680c54026d42ceb0873
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2022-09-28 08:29:12 -0300

    Update index.js

--- Curso6_NodeJS_Criando sua Biblioteca/index.js
+++ Curso6_NodeJS_Criando sua Biblioteca/index.js
@@ -11,7 +11,7 @@ function extrairLink(texto) {
   const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
   const capturas = [...texto.matchAll(regex)]; ///spread operator para expandir o conteúdo iterável
   const resultados = capturas.map((captura) => ({ [captura[1]]: captura[2] }));
-  return resultados;
+  return resultados.length !== 0 ? resultados : 'Não há links no arquivo';
 }
 
 
@@ -20,10 +20,10 @@ async function pegarArquivo(caminhoDoArquivo) {
   try {
     const encoding = "utf-8";
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding); //pegando a resposta
-    console.log(extrairLink(texto));
+    return extrairLink(texto);
   } catch (erro) {
     tratarErro(erro);
   }
 }
 
-pegarArquivo("./arquivos/texto.md");
+export default pegarArquivo;
