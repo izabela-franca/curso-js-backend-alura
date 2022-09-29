27fdb479124ff289b9e6d344cb52c5bf4bb7f467
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2022-09-29 11:01:45 -0300

    Update cli.js

--- Curso6_NodeJS_Criando sua Biblioteca/src/cli.js
+++ Curso6_NodeJS_Criando sua Biblioteca/src/cli.js
@@ -3,18 +3,29 @@
 import chalk from "chalk";
 import fs, { lstat } from "fs";
 import pegarArquivo from "./index.js";
+import listaValidada from "./http-validacao.js";
 
 const caminho = process.argv; //objeto do node que pegará os valores de argumento
 
-function imprimirLista(resultado, identificador = '') {
-  console.log(
-    chalk.yellow("Lista de links"), 
-    chalk.black.bgGreen(identificador),
-    resultado);
+async function imprimirLista(valida, resultado, identificador = '') {
+  
+  if (valida) {
+    console.log(
+      chalk.yellow("Lista validada"), 
+      chalk.black.bgGreen(identificador),
+      await listaValidada(resultado));
+  } else {
+    console.log(
+      chalk.yellow("Lista de links"), 
+      chalk.black.bgGreen(identificador),
+      resultado);
+  }
+  
 }
 
 async function processarTexto(argumentos) {
   const caminho = argumentos[2];
+  const valida = argumentos[3] === '--validar';
 
   //Tratamento de erros
   try {
@@ -29,12 +40,12 @@ async function processarTexto(argumentos) {
   //Verificando se temos um diretório ou arquivo
   if (fs.lstatSync(caminho).isFile()) {
     const resultado = await pegarArquivo(argumentos[2]);
-    imprimirLista(resultado);
+    imprimirLista(valida, resultado);
   } else if (fs.lstatSync(caminho).isDirectory()) {
     const arquivos = await fs.promises.readdir(caminho);
     arquivos.forEach(async (nomeDeArquivo) => {
       const lista = await pegarArquivo(`${caminho}/${nomeDeArquivo}`);
-      imprimirLista(lista, nomeDeArquivo);
+      imprimirLista(valida, lista, nomeDeArquivo);
     });
   }
 }
