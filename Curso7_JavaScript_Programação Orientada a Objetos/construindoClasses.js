8007f205cb20d7ffbff3d80f94203d26e2f0cbf0
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2022-10-06 11:00:45 -0300

    Update construindoClasses.js

--- Curso7_JavaScript_Programação Orientada a Objetos/construindoClasses.js
+++ Curso7_JavaScript_Programação Orientada a Objetos/construindoClasses.js
@@ -1,19 +1,22 @@
 //Construindo uma classe User
 export default class User {
+  //atributos privados
+  #nome
+  #email
+  #nascimento
+  #role
+  #ativo
+
   constructor(nome, email, nascimento, role, ativo = true) {
-    this.nome = nome;
-    this.email = email;
-    this.nascimento = nascimento;
-    this.role = role || "estudante";
-    this.ativo = ativo;
+    this.#nome = nome;
+    this.#email = email;
+    this.#nascimento = nascimento;
+    this.#role = role || "estudante";
+    this.#ativo = ativo;
   }
 
   //Criando métodos para uma classe
   exibirInfos() {
-    return `${this.nome}, ${this.email}`;
+    return `${this.#nome}, ${this.#email}`;
   }
 }
-
-//Criando um novo usuário a partir da classe contruída
-const novoUser = new User("Izabela", "izabela@email.com", "1991-05-03");
-
